b72125ccd13481e12aa7ecbfb195a98954584508
Gioelinz <gioelerunza.gr@gmail.com>
2022-01-28 15:29:34 +0100

    create list

--- js/script.js
+++ js/script.js
@@ -4,4 +4,24 @@ Vue.config.devtools = true;
 
 const app = new Vue({
     el: '#root',
+    data: {
+        tasks: [
+            {
+                text: "Fare la spesa",
+                isdone: false,
+            },
+            {
+                text: "Uscire il cane",
+                isdone: false,
+            },
+            {
+                text: "Cucinare",
+                isdone: false,
+            },
+            {
+                text: "Formattare il PC di Nonno",
+                isdone: false,
+            },
+        ]
+    }
 });
